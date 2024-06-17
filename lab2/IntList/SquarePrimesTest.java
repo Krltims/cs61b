b5217c72a34d4f66a729916fdf6b77f54d603ee1
Krltims <13916434108@163.com>
2024-06-18 00:07:02 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,39 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testnoprime() {
+        IntList lst = IntList.of(2,4,6,8);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 6 -> 8", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testnonegativeprime() {
+        IntList lst = IntList.of(0,-1,5,6,8);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> -1 -> 25 -> 6 -> 8", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testallzero() {
+        IntList lst = IntList.of(0,0,0,0);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 0 -> 0 -> 0", lst.toString());
+        assertFalse(changed);
+    }
+    @Test
+    public void testemptylist() {
+        IntList lst = IntList.of(0);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0", lst.toString());
+        assertFalse(changed);
+    }
+    @Test
+    public void regularlist() {
+        IntList lst = IntList.of(1,2,3,4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4", lst.toString());
+        assertTrue(changed);
+    }
 }
