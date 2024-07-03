b9b62ace043b3271e80d6c6af14a864a7d87903a
Krltims <13916434108@163.com>
2024-07-03 20:59:54 +0800

    2

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -90,17 +90,21 @@ package deque;
 
     //  Removes and returns the item at the front of the deque. If no such item exists, returns null.
     public T removeFirst(){
+        if (size==0){
+            return null;
+        }
         head=plus_one(head);
         size-=1;
-        items[head]=null;
         return items[head];
     }
 
     //  Removes and returns the item at the back of the deque. If no such item exists, returns null.
     public T removeLast(){
+        if (size==0){
+            return null;
+        }
         tail=minus_one(tail);
         size-=1;
-        items[tail]=null;
         return items[tail];
     }
 
