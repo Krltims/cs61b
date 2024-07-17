bb02a088cfa0abc7b0c32ae327e4b742b1706427
Krltims <13916434108@163.com>
2024-07-17 21:38:49 +0800

    lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -3,11 +3,13 @@ package flik;
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        int j = 0;
+        while(i<500){
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
+            j=++i;
         }
         System.out.println("i is " + i);
     }
---
+++ lab4/flik/Test_Set.java
@@ -0,0 +1,11 @@
+package flik;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test_Set {
+    @Test
+    public void test1() {
+        assertEquals(Flik.isSameNumber(130,130),true);
+    }
+}
